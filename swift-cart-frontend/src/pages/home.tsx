036deab36e4a03db8fe28fa616acd7458be8fb10
motiullahsajit <sajitmotiullah@gmail.com>
2024-01-31 00:10:05 +0600

    productcard added

--- swift-cart-frontend/src/pages/home.tsx
+++ swift-cart-frontend/src/pages/home.tsx
@@ -1,5 +1,29 @@
+import { Link } from "react-router-dom";
+import ProductCard from "../components/product-card";
+
 const Home = () => {
-  return <div>home</div>;
+  const addToCartHandler = () => {};
+  return (
+    <div className="home">
+      <section></section>
+      <h1>
+        Latest Products
+        <Link to="/search" className="findmore">
+          More
+        </Link>
+      </h1>
+      <main>
+        <ProductCard
+          productId="123df"
+          name="Macbook"
+          price={999}
+          stock={123}
+          photo="https://m.media-amazon.com/images/I/71jG+e7roXL._AC_SX522_.jpg"
+          handler={addToCartHandler}
+        />
+      </main>
+    </div>
+  );
 };
 
 export default Home;
