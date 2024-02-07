c3e84ccf6ef4e3ac89e9d5e3072206f8da2d2d80
motiullahsajit <sajitmotiullah@gmail.com>
2024-02-07 23:07:33 +0600

    product apis added

--- swift-cart-backend/dist/controllers/product.js
+++ swift-cart-backend/dist/controllers/product.js
@@ -2,6 +2,7 @@ import { TryCatch } from "../middlewares/error.js";
 import { Product } from "../models/product.js";
 import ErrorHandler from "../utils/utility-class.js";
 import { rm } from "fs";
+// import { faker } from "@faker-js/faker";
 export const newProduct = TryCatch(async (req, res, next) => {
     const { name, price, stock, category } = req.body;
     const photo = req.file;
@@ -37,3 +38,100 @@ export const getAdminProducts = TryCatch(async (req, res, next) => {
     const products = await Product.find({});
     return res.status(200).json({ success: true, products });
 });
+export const getSingleProduct = TryCatch(async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    if (!product)
+        return next(new ErrorHandler("Product Not Found", 404));
+    return res.status(200).json({ success: true, product });
+});
+export const updateProduct = TryCatch(async (req, res, next) => {
+    const { id } = req.params;
+    const { name, price, stock, category } = req.body;
+    const photo = req.file;
+    const product = await Product.findById(id);
+    if (!product)
+        return next(new ErrorHandler("Product Not Found", 404));
+    if (photo) {
+        rm(product.photo, () => {
+            console.log("Old Photo Deleted");
+        });
+        product.photo = photo.path;
+    }
+    if (name)
+        product.name = name;
+    if (price)
+        product.price = price;
+    if (stock)
+        product.stock = stock;
+    if (category)
+        product.category = category;
+    await product.save();
+    return res.status(200).json({
+        success: true,
+        message: "Product Updated Successfully",
+    });
+});
+export const deleteProduct = TryCatch(async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    if (!product)
+        return next(new ErrorHandler("Product Not Found", 404));
+    rm(product.photo, () => {
+        console.log("Product Photo Deleted");
+    });
+    await Product.deleteOne();
+    return res
+        .status(200)
+        .json({ success: true, message: "Product Deleted Successfully" });
+});
+export const getAllProducts = TryCatch(async (req, res, next) => {
+    const { search, sort, category, price } = req.query;
+    const page = Number(req.query.page) || 1;
+    const limit = Number(process.env.PRODUCT_PER_PAGE) || 8;
+    const skip = (page - 1) * limit;
+    const baseQuery = {};
+    if (search)
+        baseQuery.name = {
+            $regex: search,
+            $options: "i",
+        };
+    if (price)
+        baseQuery.price = { $lte: Number(price) };
+    if (category)
+        baseQuery.category = category;
+    const productsPromise = Product.find(baseQuery)
+        .sort(sort && { price: sort === "asc" ? 1 : -1 })
+        .limit(limit)
+        .skip(skip);
+    const [products, filteredOnlyProduct] = await Promise.all([
+        productsPromise,
+        Product.find(baseQuery),
+    ]);
+    const totalPage = Math.ceil(filteredOnlyProduct.length / limit);
+    return res.status(200).json({ success: true, products, totalPage });
+});
+// const generateRandomProducts = async (count: number = 10) => {
+//   const products = [];
+//   for (let i = 0; i < count; i++) {
+//     const product = {
+//       name: faker.commerce.productName(),
+//       photo: "uploads\\5ba9bd91-b89c-40c2-bb8a-66703408f986.png",
+//       price: faker.commerce.price({ min: 1500, max: 80000, dec: 0 }),
+//       stock: faker.commerce.price({ min: 0, max: 100, dec: 0 }),
+//       category: faker.commerce.department(),
+//       createdAt: new Date(faker.date.past()),
+//       updatedAt: new Date(faker.date.recent()),
+//       __v: 0,
+//     };
+//     products.push(product);
+//   }
+//   await Product.create(products);
+//   console.log({ succecss: true });
+// };
+// const deleteRandomsProducts = async (count: number = 10) => {
+//   const products = await Product.find({}).skip(2);
+//   for (let i = 0; i < products.length; i++) {
+//     const product = products[i];
+//     await product.deleteOne();
+//   }
+//   console.log({ succecss: true });
+// };
--- swift-cart-backend/dist/routes/product.js
+++ swift-cart-backend/dist/routes/product.js
@@ -1,10 +1,16 @@
 import express from "express";
 import { adminOnly } from "../middlewares/auth.js";
 import { singleUpload } from "../middlewares/multer.js";
-import { getAdminProducts, getAllCategories, getLatestProducts, newProduct, } from "../controllers/product.js";
+import { deleteProduct, getAdminProducts, getAllCategories, getAllProducts, getLatestProducts, getSingleProduct, newProduct, updateProduct, } from "../controllers/product.js";
 const app = express.Router();
 app.post("/new", adminOnly, singleUpload, newProduct);
+app.get("/all", getAllProducts);
 app.get("/latest", getLatestProducts);
 app.get("/categories", getAllCategories);
-app.get("/admin-products", getAdminProducts);
+app.get("/admin-products", adminOnly, getAdminProducts);
+app
+    .route("/:id")
+    .get(getSingleProduct)
+    .put(adminOnly, singleUpload, updateProduct)
+    .delete(adminOnly, deleteProduct);
 export default app;
