c7c3009f639d3141d0deea51b2cde239a734dbac
motiullahsajit <sajitmotiullah@gmail.com>
2024-02-24 20:52:22 +0600

    orders api added

--- swift-cart-frontend/src/redux/store.ts
+++ swift-cart-frontend/src/redux/store.ts
@@ -3,6 +3,7 @@ import { userAPI } from "./api/userAPI";
 import { userReducer } from "./reducer/userReducer";
 import { productAPI } from "./api/productAPI";
 import { cartReducer } from "./reducer/cartReducer";
+import { orderApi } from "./api/orderAPI";
 
 export const server = import.meta.env.VITE_SERVER;
 
@@ -10,6 +11,7 @@ export const store = configureStore({
   reducer: {
     [userAPI.reducerPath]: userAPI.reducer,
     [productAPI.reducerPath]: productAPI.reducer,
+    [orderApi.reducerPath]: orderApi.reducer,
     [userReducer.name]: userReducer.reducer,
     [cartReducer.name]: cartReducer.reducer,
   },
