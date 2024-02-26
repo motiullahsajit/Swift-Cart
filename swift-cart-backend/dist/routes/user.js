05485ddec6e55782e55e4f5cefea12c2675a64c8
motiullahsajit <sajitmotiullah@gmail.com>
2024-02-26 19:46:26 +0600

    minor updates

--- swift-cart-backend/dist/routes/user.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { deleteUser, getAllUsers, getUser, newUser, } from "../controllers/user.js";
-import { adminOnly } from "../middlewares/auth.js";
-const app = express.Router();
-app.post("/new", newUser);
-app.get("/all", adminOnly, getAllUsers);
-app.route("/:id").get(getUser).delete(adminOnly, deleteUser);
-export default app;
