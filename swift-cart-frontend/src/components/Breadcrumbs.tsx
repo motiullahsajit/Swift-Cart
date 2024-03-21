14c91ed3397f990f059c3512da74933e69fe3e39
Mahmudul Hasan Anabil <hasanabilna@gmail.com>
2024-03-21 17:49:59 +0600

    breadcrumbs

--- swift-cart-frontend/src/components/Breadcrumbs.tsx
+++ swift-cart-frontend/src/components/Breadcrumbs.tsx
@@ -2,13 +2,12 @@ import { useLocation } from "react-router-dom";
 
 const Breadcrumbs = () => {
   const location = useLocation();
-  console.log(location);
 
-  let currentPath = location.pathname;
+  const currentPath = location.pathname;
 
   return (
-    <div>
-      <h1>Breadcrumbs</h1>
+    <div className="w-[1320px] mx-auto">
+      <h1>All &gt; {currentPath}</h1>
     </div>
   );
 };
